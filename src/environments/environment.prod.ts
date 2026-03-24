f785397cd9e46efb9ef25a9183e57579ab453dd0
Daniel Hernandez <danielalejandro627@gmail.com>
2026-03-23 19:35:15 -0500

    Actualizacion variables de entorno

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ export const environment = {
   apiUrl: 'https://api.your-domain.com',
   apiRootUrlBase: 'api/v1',
   firebase: {
-    apiKey: 'AIzaSyC6RAbKRc5x2mLWurwlWWLUNztUXgeyuMI',
-    authDomain: 'fifa-tournament-5f3c0.firebaseapp.com',
-    projectId: 'fifa-tournament-5f3c0',
-    storageBucket: 'fifa-tournament-5f3c0.appspot.com',
-    messagingSenderId: '547328550174',
-    appId: '1:547328550174:web:ec5d482759d11145a0150f',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
   },
 };
